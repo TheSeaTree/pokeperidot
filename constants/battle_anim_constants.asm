0642454bc795179fa0d8d3a1007100223d8d7694
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2019-06-22 10:21:13 -0400

    Add Animation Object for Dragon Dance

--- constants/battle_anim_constants.asm
+++ constants/battle_anim_constants.asm
@@ -220,6 +220,7 @@ BATTLEANIM_BASE_TILE EQU 7 * 7  ; Maximum size of a pokemon picture
 	const ANIM_OBJ_ENEMYFEET_2ROW
 	const ANIM_OBJ_PLAYERHEAD_2ROW
 	const ANIM_OBJ_COIL
+	const ANIM_OBJ_DRAGON_DANCE
 
 ; DoBattleAnimFrame arguments (see engine/battle_anims/functions.asm)
 	const_def
