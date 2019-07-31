60ae39ecd0a1b132bed03fd4096cd1963e51621d
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2019-07-31 12:20:56 -0400

    Update Route12EcruteakGate.asm

--- maps/Route12EcruteakGate.asm
+++ maps/Route12EcruteakGate.asm
@@ -9,8 +9,8 @@ Route12EcruteakGate_MapEvents:
 	db 0, 0 ; filler
 
 	db 4 ; warp events
-	warp_event  0,  4, ECRUTEAK_CITY, 6
-	warp_event  0,  5, ECRUTEAK_CITY, 7
+	warp_event  0,  4, ECRUTEAK_CITY, 5
+	warp_event  0,  5, ECRUTEAK_CITY, 6
 	warp_event  9,  4, ROUTE_12, 1
 	warp_event  9,  5, ROUTE_12, 2
 
