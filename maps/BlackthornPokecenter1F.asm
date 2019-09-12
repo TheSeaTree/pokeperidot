ef04f85c82dd933cc075fda4ed3994344a9bcdca
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2019-09-12 12:20:35 -0400

    Update BlackthornPokecenter1F.asm

--- maps/BlackthornPokecenter1F.asm
+++ maps/BlackthornPokecenter1F.asm
@@ -12,11 +12,14 @@ BlackthornPokecenter1FNurseScript:
 BlackthornPokecenter1F_MapEvents:
 	db 0, 0 ; filler
 
-	db 0 ; warp events
+	db 3 ; warp events
+	warp_event  3,  7, BLACKTHORN_CITY, 2
+	warp_event  4,  7, BLACKTHORN_CITY, 2
+	warp_event  0,  7, POKECENTER_2F, 1	
 
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
 	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornPokecenter1FNurseScript, -1
