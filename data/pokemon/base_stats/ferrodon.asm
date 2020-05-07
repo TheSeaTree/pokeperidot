0f5d2ab9a8f1ece12657a5b1ed64946098cd20ae
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2020-05-07 00:49:56 -0400

    Wild Pokemon held item update

--- data/pokemon/base_stats/ferrodon.asm
+++ data/pokemon/base_stats/ferrodon.asm
@@ -6,7 +6,7 @@
 	db STEEL, DRAGON ; type
 	db 45 ; catch rate
 	db 218 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db NO_ITEM, METAL_COAT ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 1 ; step cycles to hatch
