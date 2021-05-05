7a484f8043631f1cf1f1fd7a3fc40af3fe596881
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2021-05-05 06:24:14 -0400

    Text String Fix
    Twin Faye's after-battle text was too long for the text box.

--- maps/IlexForest.asm
+++ maps/IlexForest.asm
@@ -265,8 +265,8 @@ TwinsFayeAndMaeWinText:
 
 TwinFayeAfterText:
 	text "FAYE: I think my"
-	line "#MON was stronger"
-	cont "than MAE's."
+	line "#MON was stron="
+	cont "ger than MAE's."
 	done
 
 TwinMaeText:
