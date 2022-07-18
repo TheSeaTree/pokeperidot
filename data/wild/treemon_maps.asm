4b2f694cfdc4e61ef34bdad2c8d89a6f7a00c3dc
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2022-07-18 10:13:34 -0400

    Missing Headbutt Tree Encounters

--- data/wild/treemon_maps.asm
+++ data/wild/treemon_maps.asm
@@ -7,6 +7,7 @@ TreeMonMaps:
 	treemon_map ELKHORN_TOWN,              TREEMON_SET_TOWN
 	treemon_map ROUTE_1,                   TREEMON_SET_ROUTE
 	treemon_map PAVONA_VILLAGE,            TREEMON_SET_TOWN
+	treemon_map ROUTE_30,                  TREEMON_SET_LAKE
 	treemon_map ROUTE_2,                   TREEMON_SET_FOREST
 	treemon_map PECTINIA_CITY,             TREEMON_SET_CITY
 	treemon_map ROUTE_3,                   TREEMON_SET_ROUTE
@@ -32,13 +33,31 @@ TreeMonMaps:
 	treemon_map ROUTE_20,                  TREEMON_SET_ROUTE
 	treemon_map ACROPORA_CITY,             TREEMON_SET_TOWN
 	treemon_map ROUTE_21,                  TREEMON_SET_ROUTE
+	treemon_map ROUTE_23,                  TREEMON_SET_ROUTE
+	treemon_map ROUTE_24_NORTH,            TREEMON_SET_FOREST
+	treemon_map ROUTE_24_SOUTH,            TREEMON_SET_FOREST
+	treemon_map ROUTE_25,	               TREEMON_SET_FOREST
+	treemon_map ROUTE_26,	               TREEMON_SET_LAKE
+	treemon_map ROUTE_27,	               TREEMON_SET_ROUTE
+	treemon_map ROUTE_28,	               TREEMON_SET_FOREST
+	treemon_map NORTH_FOREST,              TREEMON_SET_FOREST
 	db -1
 
 RockMonMaps:
 	treemon_map PAVONA_VILLAGE,            TREEMON_SET_ROCK
 	treemon_map ROUTE_3,                   TREEMON_SET_ROCK
+	treemon_map ROUTE_4,                   TREEMON_SET_ROCK
+	treemon_map ROUTE_5,           		   TREEMON_SET_KANTO
 	treemon_map ROUTE_6,           		   TREEMON_SET_KANTO
 	treemon_map SEA_ROUTE_4,       		   TREEMON_SET_KANTO
 	treemon_map ROUTE_8,           		   TREEMON_SET_KANTO
 	treemon_map RUGOSA_COAST,      		   TREEMON_SET_KANTO
+	treemon_map FACTORY_1F,      		   TREEMON_SET_ROCK
+	treemon_map FACTORY_2F,      		   TREEMON_SET_ROCK
+	treemon_map HEPATICA_TOWN,     		   TREEMON_SET_ROCK
+	treemon_map PALEROCK_MOUNTAIN_1F,	   TREEMON_SET_ROCK
+	treemon_map REMNANT_CAVE_3F,    	   TREEMON_SET_ROCK
+	treemon_map ROUTE_13,		    	   TREEMON_SET_ROCK
+	treemon_map ROUTE_26_CAVE_B1F,    	   TREEMON_SET_ROCK
+	treemon_map UNION_CAVE_1F,      	   TREEMON_SET_ROCK
 	db -1
