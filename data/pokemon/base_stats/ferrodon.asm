60185bd21f122b5fd1c76485df683c6c9e302d43
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2023-12-03 07:45:36 -0500

    Fix Ferrodon's Egg Step Cycle Count

--- data/pokemon/base_stats/ferrodon.asm
+++ data/pokemon/base_stats/ferrodon.asm
@@ -9,7 +9,7 @@
 	db NO_ITEM, METAL_COAT ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 1 ; step cycles to hatch
+	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/ferrodon/front.dimensions"
 	db 0, 0, 0, 0 ; padding
