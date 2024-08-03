049fcbe4df5065fc7110d0f4d8813114c6ae6344
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2024-08-03 18:11:40 -0400

    Make Roof Tiles Consistent

--- data/maps/roofs.asm
+++ data/maps/roofs.asm
@@ -17,35 +17,35 @@ MapGroupRoofs:
 	db ROOF_VIOLET    ;  4 (Staghorn)
 	db ROOF_AZALEA    ;  5 (Acropora)
 	db ROOF_AZALEA    ;  6 (Safari)
-	db -1             ;  7
+	db -1             ;  7 (Carnation)
 	db ROOF_GOLDENROD ;  8 (Pectinia)
-	db ROOF_AZALEA    ;  9 (East Forest)
+	db ROOF_AZALEA    ;  9 (Orientem)
 	db ROOF_OLIVINE   ; 10 (Ridge)
-	db ROOF_AZALEA    ; 11 (Favia)
-	db -1             ; 12
-	db ROOF_AZALEA    ; 13 (Route 11)
+	db ROOF_NEW_BARK  ; 11 (Favia)
+	db -1             ; 12 (Dept Store)
+	db ROOF_AZALEA    ; 13 (Route 10)
 	db -1             ; 14 (Roofs)
-	db -1             ; 15
+	db -1             ; 15 (SS Mako)
 	db ROOF_OLIVINE   ; 16 (Pokemon League)
-	db -1             ; 17
-	db -1             ; 18
-	db ROOF_NEW_BARK  ; 19
+	db -1             ; 17 (Occident)
+	db -1             ; 18 (Route 11)
+	db ROOF_NEW_BARK  ; 19 (Lost Land)
 	db -1             ; 20 (Cable Club)
-	db -1             ; 21
+	db -1             ; 21 (Castle)
 	db ROOF_SNOWY     ; 22 (Hepatica)
-	db -1             ; 23 (Orchid)
+	db ROOF_NEW_BARK  ; 23 (Orchid)
 	db ROOF_AZALEA    ; 24 (Elkhorn)
-	db -1             ; 25
-	db ROOF_NEW_BARK  ; 26 (Pavona)
+	db -1             ; 25 (Postgame)
+	db ROOF_AZALEA    ; 26 (Pavona)
 	db ROOF_SNOWY	  ; 26 (Palerock)
-	db -1			  ; 27 (North Forest)
+	db -1			  ; 27 (Boreas Forest)
 	db ROOF_OLIVINE   ; 28 (Sea Route 7)
 	db -1			  ; 29 (Battle Sim)
-	db ROOF_AZALEA	  ; 29 (Past Favia)
-	db ROOF_GOLDENROD ; 29 (Past Pectinia)
-	db ROOF_AZALEA	  ; 29 (Past Route 11)
-	db ROOF_AZALEA	  ; 29 (Past Elkhorn)
-	db ROOF_NEW_BARK  ; 29 (Past Pavona)
+	db ROOF_NEW_BARK  ; 30 (Past Favia)
+	db ROOF_GOLDENROD ; 31 (Past Pectinia)
+	db ROOF_AZALEA	  ; 32 (Past Route 10)
+	db ROOF_AZALEA	  ; 33 (Past Elkhorn)
+	db ROOF_AZALEA    ; 34 (Past Pavona)
 
 Roofs:
 ; entries correspond to ROOF_* constants
