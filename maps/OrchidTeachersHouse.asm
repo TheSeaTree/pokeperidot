7ebbd9415adb816cc4c2b9ce2c338818e0e9f1a4
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2024-10-29 13:54:25 -0400

    Amulet Coin Event Fixes
    Use a temporary event so the teacher NPC does not repeat the same script after talking to her multiple times in one visit.
    Have her face the player when gifting the Amulet Coin.

--- maps/OrchidTeachersHouse.asm
+++ maps/OrchidTeachersHouse.asm
@@ -1,5 +1,6 @@
 	const_def 2 ; object constants
-	const ORCHIDTEACHERSHOUSE_TEACHER
+	const ORCHIDTEACHERSHOUSE_TEACHER_MORN
+	const ORCHIDTEACHERSHOUSE_TEACHER_DAY
 
 OrchidTeachersHouse_MapScripts:
 	db 0 ; scene scripts
@@ -7,24 +8,31 @@ OrchidTeachersHouse_MapScripts:
 	db 0 ; callbacks
 
 OrchidTeacherDaytime:
-	opentext
+	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
+	iftrue .AlreadySeen
 	checkevent EVENT_AMULET_COIN_FROM_ORCHID_TEACHER
 	iftrue .GotAmuletCoin
 	checkevent EVENT_MET_ORCHID_TEACHER_OUTSIDE
 	iftrue .GiveAmuletCoin
+	opentext
 	writetext OrchidTeacherSighText
 	waitbutton
 	closetext
 	faceplayer
-	showemote EMOTE_QUESTION, ORCHIDTEACHERSHOUSE_TEACHER, 8
+	showemote EMOTE_QUESTION, ORCHIDTEACHERSHOUSE_TEACHER_DAY, 8
 	opentext
 	writetext OrchidTeacherNoticeText
 	waitbutton
+	writetext OrchidTeacherAfterNoticeText
+	waitbutton
 	setevent EVENT_MET_ORCHID_TEACHER_IN_HOUSE
 	closetext
+	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
 	end
 
 .GiveAmuletCoin
+	faceplayer
+	opentext
 	writetext OrchidTeacherGiftText
 	waitbutton
 	verbosegiveitem AMULET_COIN
@@ -36,19 +44,19 @@ OrchidTeacherDaytime:
 	end
 
 .GotAmuletCoin
-	writetext OrchidCityAfterGiftText
-	waitbutton
-	closetext
-	end
+	jumptextfaceplayer OrchidCityAfterGiftText
 	
 .NoRoom
 	writetext OrchidTeacherNoRoomText
 	waitbutton
 	closetext
 	end
-	
+
+.AlreadySeen
+	jumptextfaceplayer OrchidTeacherAfterNoticeText
+
 OrchidTeacherMorning:
-	showemote EMOTE_SLEEP, ORCHIDTEACHERSHOUSE_TEACHER, 15
+	showemote EMOTE_SLEEP, ORCHIDTEACHERSHOUSE_TEACHER_MORN, 15
 	opentext
 	writetext OrchidTeacherSleepingText
 	waitbutton
@@ -114,10 +122,12 @@ OrchidTeacherNoticeText:
 	line "here to ORCHID, it"
 	cont "has been tough to"
 	cont "meet new friends."
-	
-	para "You're not from"
+	done
+
+OrchidTeacherAfterNoticeText:
+	text "You're not from"
 	line "here, are you?"
-	
+
 	para "You should see the"
 	line "lava at night, it's"
 	cont "so pretty!"
@@ -125,7 +135,7 @@ OrchidTeacherNoticeText:
 	para "…Plus, I would"
 	line "enjoy the company."
 	done
-	
+
 OrchidTeacherSleepingText:
 	text "…Zzzz…"
 
