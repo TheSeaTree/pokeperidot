dcb63e71ecd3112d23e426b9c083fe1ad5a142a3
TheSeaTree <44342313+TheSeaTree@users.noreply.github.com>
2026-03-16 14:28:57 -0400

    Update cave_collision.asm
    Fix incorrect tile collision

--- data/tilesets/cave_collision.asm
+++ data/tilesets/cave_collision.asm
@@ -207,7 +207,7 @@
 	tilecoll UP_WALL, WALL, FLOOR, WALL ; ce
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; cf
 	tilecoll WALL, UP_WALL, WALL, FLOOR ; d0
-	tilecoll UP_WALL, UP_WALL, LADDER, WALL ; d1
+	tilecoll UP_WALL, WALL, LADDER, WALL ; d1
 	tilecoll WALL, WALL, WALL, WALL ; d2
 	tilecoll WALL, WALL, WALL, WALL ; d3
 	tilecoll FLOOR, FLOOR, FLOOR, WARP_CARPET_DOWN ; d4
